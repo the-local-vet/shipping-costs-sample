da04534b6db61ea8c92a49cdf63ad41e3cbd5fbe
Oli123456 <31634286+Oli123456@users.noreply.github.com>
2017-09-13 00:08:45 +0100

    ojus changes to line 54 etc.

--- app.py
+++ app.py
@@ -51,7 +51,9 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
     print "Starting app on port %d" % port
 
